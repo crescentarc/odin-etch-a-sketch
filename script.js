da985c461c712196ad76d1e2798fe27ebcb298dc
crescentarc <matthew.seth.taganile@gmail.com>
2022-12-21 17:49:00 -0500

    Add range slider to adjust grid dimensions

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-function createGrid(size) {
+let gridContainer = document.querySelector('#grid-container')
+
+//allows one to change dimensions of grid with slider
+function sizeSelector (){
+    let input = document.querySelector(`#input`);
+    let display = document.querySelector(`#display`);
+    display.innerText = input.value + `x` + input.value;
+
+    //displays current dimensions next to slider (changes as mouse is held down)
+    input.oninput = function() {
+        display.innerText = input.value + `x` + input.value;
+    }
+
+    //clears out current grid and updates dimensions (changes once mouse is released)
+    input.onchange = function() {
+        gridContainer.innerHTML = ``;
+        let size = input.value;
+        createGrid(size);
+    }
+
+    let size = input.value;
+    createGrid(size);
+}
+
+//generates grid (dimensions are 16x16 by default based on range slider)
+function createGrid(size){
 
     let gridContainer = document.querySelector(`#grid-container`);
 
@@ -17,4 +42,5 @@ function createGrid(size) {
     }
 }
 
-createGrid(16);
+//loads page with default 16x16 slider
+sizeSelector();
